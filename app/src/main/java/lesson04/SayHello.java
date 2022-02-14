8c9082ca761c9eae1c821deda5351c41632f23d8
Shane K. Panter <shane@foundationcode.com>
2022-02-14 14:29:55 -0700

    Initial commit

---
+++ app/src/main/java/lesson04/SayHello.java
@@ -0,0 +1,10 @@
+package lesson04;
+
+public class SayHello implements Talker{
+
+    @Override
+    public String SaySomething() {
+        return null;
+    }
+
+}
---
+++ app/src/test/java/lesson04/SayWorldTest.java
@@ -0,0 +1,13 @@
+package lesson04;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class SayWorldTest {
+
+    @Test
+    public void methodReturnsWorld() {
+        assert(false); //Remove this!!
+    }
+    
+}
